a96e3900cae2de500cf612e56d4c101f7c49bd84
Christian Cosgrove <christian.cosgrove@gmail.com>
2017-10-10 21:24:29 -0400

    positive and negative minibatch generation

--- main.py
+++ main.py
@@ -17,32 +17,30 @@ chunk_size = 1
 from numpy import genfromtxt
 class_list = [x.decode('ascii') for x in genfromtxt('classes.csv', delimiter=',', dtype=None)]
 
+ITERATIONS = 100000
+
 def main():
 	reader = DataReader(images_directory, width, width, channels, class_list)
 
 	model = SSLModel(width, width, channels, mb_size, len(class_list))
 	Thread(target=lambda: start_server(reader, model)).start()
 
-	for e in range(1000):
-
-		c = e % 3
+	for e in range(ITERATIONS):
 
 		t = time.time()
-		chunk_lab = reader.minibatch_labeled(chunk_size * mb_size, c)
+		chunk_lab = reader.minibatch_labeled(chunk_size * mb_size)
 		chunk_unl = reader.minibatch_unlabeled(chunk_size * mb_size)
 		t = time.time()
 		if chunk_lab is None:
 			continue
 		else:
 			i = 0
-			# for i in range(chunk_size):
 			X_u = chunk_unl[i * mb_size : (i+1) * mb_size]
 			X_l = chunk_lab[0][i * mb_size : (i+1) * mb_size]
 			Y = chunk_lab[1][i * mb_size : (i+1) * mb_size]
-			# if X_u.shape[0] != mb_size or X_l.shape[0] != mb_size or Y.shape[0] != mb_size:
-			# 	continue
-			dloss, gloss = model.train_step(X_u, X_l, Y, c)
+			dloss, gloss = model.train_step(X_u, X_l, Y)
 			print('.', end='', flush=True)
+
 
 			if e % 100 == 0:
 				print(dloss, gloss)
